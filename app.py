2e27f15d131074884f761a320256fe82eb3988b2
balbino aylagas <balbino_a@hotmail.com>
2022-01-26 18:28:37 -0600

    instructions

---
+++ app.py
@@ -0,0 +1,10 @@
+"""
+una aplicacion
+debe ser una aplicacion que pueda sumar cualquier cantidad de numeros y
+luego dar la suma.
+
+la aplicacion debe tener un menu presentado al usuario en consola y
+debe tener las opciones de agregar un nuevo numero y mostrar
+la suma de los numeros que se tienen
+debe tener opcion para terminar el programa
+"""
