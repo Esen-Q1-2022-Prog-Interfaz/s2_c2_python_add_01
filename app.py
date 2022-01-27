fb79e4ebe96c159e4be2f8cb9928e4d716b6bcea
balbino aylagas <balbino_a@hotmail.com>
2022-01-26 18:45:17 -0600

    app is working with no logic yet

--- app.py
+++ app.py
@@ -7,4 +7,21 @@ la aplicacion debe tener un menu presentado al usuario en consola y
 debe tener las opciones de agregar un nuevo numero y mostrar
 la suma de los numeros que se tienen
 debe tener opcion para terminar el programa
+
+ 0:: agregar un nuevo numero
+ 1:: mostrar la suma
+-1:: exit
+
 """
+result = 0
+
+while True:
+    option = int(input("option: "))
+    if option == -1:
+        break
+    elif option == 0:
+        print("agregar numero...")
+    elif option == 1:
+        print(f"result-> {result}")
+    else:
+        print("app is working...")
